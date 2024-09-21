2437859fe7b9e990446b5ec847caa6909146952d
pclwillmott <pwillmott@northrock.bm>
2024-09-21 08:09:38 -0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SGInteger",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SGInteger",
+            targets: ["SGInteger"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SGInteger"),
+        .testTarget(
+            name: "SGIntegerTests",
+            dependencies: ["SGInteger"]
+        ),
+    ]
+)
