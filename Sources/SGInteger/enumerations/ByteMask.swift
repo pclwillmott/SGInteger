5d51c14b20002dda6708e60c332007fbbeba7294
pclwillmott <pwillmott@northrock.bm>
2024-09-26 10:55:24 -0300

    ByteMask added.

---
+++ Sources/SGInteger/enumerations/ByteMask.swift
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------------
+// ByteMask.swift
+//
+// This Swift source file is a part of the SGInteger package
+// by Paul C. L. Willmott.
+//
+// Copyright © 2024 Paul C. L. Willmott. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the “Software”), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+// -----------------------------------------------------------------------------
+//
+// Revision History:
+//
+//     26/09/2024  Paul Willmott - ByteMask.swift created
+// -----------------------------------------------------------------------------
+
+import Foundation
+
+public enum ByteMask {
+  
+  // MARK: Enumeration
+  
+  static let d0 : UInt8 = 0b00000001
+  static let d1 : UInt8 = 0b00000010
+  static let d2 : UInt8 = 0b00000100
+  static let d3 : UInt8 = 0b00001000
+  static let d4 : UInt8 = 0b00010000
+  static let d5 : UInt8 = 0b00100000
+  static let d6 : UInt8 = 0b01000000
+  static let d7 : UInt8 = 0b10000000
+
+}
