bc2b2ac6213c74cc6a13d5018e2340f6e116861a
pclwillmott <pwillmott@northrock.bm>
2024-09-26 10:58:56 -0300

    ByteMask changed to public.

--- Sources/SGInteger/enumerations/ByteMask.swift
+++ Sources/SGInteger/enumerations/ByteMask.swift
@@ -36,13 +36,13 @@ public enum ByteMask {
   
   // MARK: Enumeration
   
-  static let d0 : UInt8 = 0b00000001
-  static let d1 : UInt8 = 0b00000010
-  static let d2 : UInt8 = 0b00000100
-  static let d3 : UInt8 = 0b00001000
-  static let d4 : UInt8 = 0b00010000
-  static let d5 : UInt8 = 0b00100000
-  static let d6 : UInt8 = 0b01000000
-  static let d7 : UInt8 = 0b10000000
+  public static let d0 : UInt8 = 0b00000001
+  public static let d1 : UInt8 = 0b00000010
+  public static let d2 : UInt8 = 0b00000100
+  public static let d3 : UInt8 = 0b00001000
+  public static let d4 : UInt8 = 0b00010000
+  public static let d5 : UInt8 = 0b00100000
+  public static let d6 : UInt8 = 0b01000000
+  public static let d7 : UInt8 = 0b10000000
 
 }
